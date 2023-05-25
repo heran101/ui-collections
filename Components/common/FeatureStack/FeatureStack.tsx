68a5fb8313fa67d87132b12e1eacf6f44a6cd20e
Heran Tsegaye <herantse@gmail.com>
2023-05-25 17:00:52 -0400

    Add the feature stack

--- Components/common/FeatureStack/FeatureStack.tsx
+++ Components/common/FeatureStack/FeatureStack.tsx
@@ -1,25 +1,31 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 import styled from "styled-components";
-const FSWrapper = styled.div`
-  margin-top: 100px;
-  padding-top: 150px;
-  padding-bottom: 10px;
-  max-width: 700px;
-  margin: 0 200px;
+interface FSwrapperInterface {
+  ref: any;
+}
+const FSWrapper = styled.div<FSwrapperInterface>`
+  background: blue;
+  position: relative;
+  height: 100rem;
+  padding: 140px 0;
   font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Helvetica,
     Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol";
 `;
-
 const Card = styled.div`
-  top: 20px;
-  position: sticky;
-  border: 1px solid #ccc;
-  box-shadow: 15px 15px 15px rgba(0, 0, 0, 0.1);
-  background-color: white;
-  color: #333;
-  padding: 40px;
-  border-radius: 10px;
-  margin-bottom: 50px;
+  position: absolute;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  width: 70%;
+  height: 300px;
+  padding: 20px;
+  margin: 10px auto;
+  background-color: #f0f0f0;
+  border-radius: 48px;
+  box-shadow: 0 2px 5px rgba(0, 0, 0, 0.1);
+  font-size: 18px;
+  transition: transform 0.3s ease-in-out;
+
   h2 {
     padding: 0;
   }
@@ -29,97 +35,87 @@ const Card = styled.div`
     color: #0c4eb9;
   }
   &:nth-child(1n) {
-    top: -100px;
-    right: 20px;
+    bottom: 100px;
     background: red;
     z-index: 100000;
   }
   &:nth-child(2n) {
-    top: -80px;
+    bottom: 80px;
     z-index: 10000;
     background: blue;
   }
   &:nth-child(3n) {
     z-index: 1000;
     background: yellow;
-    top: -60px;
+    bottom: 60px;
   }
 
   &:nth-child(4n) {
     z-index: 100;
     background: green;
-    top: -40px;
+    bottom: 40px;
   }
   &:nth-child(5n) {
-    top: -20px;
+    bottom: 20px;
     z-index: 10;
     background: purple;
   }
-  &:nth-child(6n) {
-    z-index: 9;
-    top: 0;
-  }
-  &:nth-child(7n) {
-    z-index: 8;
-    top: 20px;
-  }
-  &:nth-child(8n) {
-    z-index: 7;
-    top: 40px;
-  }
 `;
-const NextSection = styled.div`
-  background: green;
-  height: 900px;
-  width: 100%;
-`;
-const FSQWrapper = styled.div`
-  height: 100vh;
-  width: 30rem;
-  margin-right: auto;
-  margin-left: auto;
-  display: grid;
-  grid-template-columns: 1fr;
-  grid-template-rows: 1fr;
-`;
-const ESCard = styled.div`
-  margin: auto;
-  width: 50%;
-  border: 1px solid #ccc;
-  box-shadow: 15px 15px 15px rgba(0, 0, 0, 0.1);
-  background-color: white;
-  color: #333;
-  padding: 40px;
-  border-radius: 10px;
-  margin-bottom: 50px;
-  h2 {
-    padding: 0;
-  }
-  span {
-    display: block;
-    font-size: 14px;
-    color: #0c4eb9;
-  }
-  &:nth-child(1n) {
-    grid-column-start: 1;
-    background: red;
-    z-index: 100000;
-  }
-  &:nth-child(2n) {
-    grid-column-start: 1;
-    background: blue;
-  }
-  &:nth-child(3n) {
-    grid-column-start: 1;
-    z-index: 1000;
-    background: yellow;
-  }
-`;
+
+interface windowDimentionsInt {
+  innerWidth: number;
+  innerHeight: number;
+}
 const FeatureStack = () => {
+  const featureStack = useRef<HTMLElement | null>(null);
+  const [scrollPosition, setScrollPosition] = useState(0);
+  const [windowDimensions, setWindowDimensions] =
+    useState<windowDimentionsInt>();
+
+  useEffect(() => {
+    const handleResize = () => {
+      const { innerWidth, innerHeight } = window;
+      setWindowDimensions({ innerWidth, innerHeight });
+    };
+    window.addEventListener("resize", handleResize);
+    handleResize();
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
+  const handleScroll = () => {
+    const currentPosition =
+      window.pageYOffset || document.documentElement.scrollTop;
+    setScrollPosition(currentPosition);
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, [handleScroll, scrollPosition]);
+
+  useEffect(() => {
+    if (!windowDimensions) return;
+    const mainWrapperRect = featureStack.current?.getBoundingClientRect();
+    const cardList = featureStack?.current?.children;
+
+    if (mainWrapperRect!.top < windowDimensions!.innerHeight - 600) {
+      if (!cardList) return;
+      for (var i = 0; i < cardList!.length; i++) {
+        const oneCard = cardList[i] as HTMLElement;
+        oneCard.style.position = "sticky";
+        oneCard.style.marginTop = "60px";
+        oneCard.style.bottom = `${100 + (i - 1) * -30}px`;
+        oneCard.style.transition = "all 30ms";
+        oneCard.style.top = `${-100 + (i - 1) * 30}px`;
+      }
+    }
+  }, [scrollPosition, windowDimensions]);
   return (
     <>
       <div>
-        <FSWrapper>
+        <FSWrapper ref={featureStack}>
           <Card>
             <h2>
               <span>Project #1</span>Title of the Project
@@ -164,77 +160,8 @@ const FeatureStack = () => {
               laborum ad a sequi!
             </p>
           </Card>
-          <Card>
-            <h2>
-              <span>Project #5</span>Title of the Project
-            </h2>
-            <p>
-              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Harum,
-              perspiciatis blanditiis accusamus commodi consectetur id tempora
-              rem iure eligendi quos eos et autem ratione exercitationem earum
-              laborum ad a sequi!
-            </p>
-          </Card>
-          <Card>
-            <h2>
-              <span>Project #6</span>Title of the Project
-            </h2>
-            <p>
-              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Harum,
-              perspiciatis blanditiis accusamus commodi consectetur id tempora
-              rem iure eligendi quos eos et autem ratione exercitationem earum
-              laborum ad a sequi!
-            </p>
-          </Card>
-          <Card>
-            <h2>
-              <span>Project #7</span>Title of the Project
-            </h2>
-            <p>
-              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Harum,
-              perspiciatis blanditiis accusamus commodi consectetur id tempora
-              rem iure eligendi quos eos et autem ratione exercitationem earum
-              laborum ad a sequi!
-            </p>
-          </Card>
         </FSWrapper>
-        <FSQWrapper>
-          <ESCard>
-            <h2>
-              <span>Project #1</span>Title of the Project
-            </h2>
-            <p>
-              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Harum,
-              perspiciatis blanditiis accusamus commodi consectetur id tempora
-              rem iure eligendi quos eos et autem ratione exercitationem earum
-              laborum ad a sequi!
-            </p>
-          </ESCard>
-          <ESCard>
-            <h2>
-              <span>Project #2</span>Title of the Project
-            </h2>
-            <p>
-              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Harum,
-              perspiciatis blanditiis accusamus commodi consectetur id tempora
-              rem iure eligendi quos eos et autem ratione exercitationem earum
-              laborum ad a sequi!
-            </p>
-          </ESCard>
-          <ESCard>
-            <h2>
-              <span>Project #3</span>Title of the Project
-            </h2>
-            <p>
-              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Harum,
-              perspiciatis blanditiis accusamus commodi consectetur id tempora
-              rem iure eligendi quos eos et autem ratione exercitationem earum
-              laborum ad a sequi!
-            </p>
-          </ESCard>
-        </FSQWrapper>
       </div>
-      <NextSection></NextSection>
     </>
   );
 };
--- pages/index.tsx
+++ pages/index.tsx
@@ -98,6 +98,7 @@ export default function Home() {
           </HorizontalCenter>
         </Container>
         <FeatureStack />
+        <FAQ />
       </main>
     </>
   );
